6dc17f4b764c870baf11a0029feea8aff61a3004
soso <soso.so0829@gmail.com>
2024-10-03 00:29:09 +0800

    add database and setup docker compose file

---
+++ backend/src/config/db.ts
@@ -0,0 +1,14 @@
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const pool = new Pool({
+  host: process.env.PGHOST || 'localhost',
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  port: parseInt(process.env.PGPORT || '5432'),
+});
+
+export default pool;
